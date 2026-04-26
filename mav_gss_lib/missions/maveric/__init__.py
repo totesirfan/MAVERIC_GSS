42bae16ff545382145c4b3ff43a43f054be941a3
Irfan <irfan.annuar18@gmail.com>
2026-04-25 17:14:31 -0700

    feat(maveric): re-export declarative capabilities from package root

--- mav_gss_lib/missions/maveric/__init__.py
+++ mav_gss_lib/missions/maveric/__init__.py
@@ -1,1 +1,8 @@
 """MAVERIC mission package for the MissionSpec runtime."""
+
+from .declarative import (
+    DeclarativeCapabilities,
+    build_declarative_capabilities,
+)
+
+__all__ = ("DeclarativeCapabilities", "build_declarative_capabilities")
